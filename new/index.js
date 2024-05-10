9f2070f74e583402c293dfc004fd25bdb4bf38bd
lsh <liushihuancoke@gmail.com>
2024-05-10 21:30:47 +0800

    feat: newObj func

---
+++ new/index.js
@@ -0,0 +1,12 @@
+
+/**
+ * newObj函数，复刻new操作符的功能
+ */
+function newObj(Func, ...args) {
+  const obj = {};
+  Func.prototype.constructor = Func;
+  Object.setPrototypeOf(obj, Func.prototype);
+
+  const result = Func.apply(obj, args);
+  return result instanceof Object ? result : obj;
+}
